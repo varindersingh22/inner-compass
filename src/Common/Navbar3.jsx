976d27641e12fadb732960de687a71dcca0f23bb
shadowvj3 <shadowvj3@gmail.com>
2024-08-12 06:07:57 +0000

    uu

--- src/Common/Navbar3.jsx
+++ src/Common/Navbar3.jsx
@@ -4,6 +4,8 @@ import './Navbar.css';
 
 function Navbar() {
   const [navBackground, setNavBackground] = useState('transparent');
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const handleScroll = () => {
     if (window.scrollY > 50) {
       setNavBackground('solid');
@@ -12,27 +14,31 @@ function Navbar() {
     }
   };
 
-  
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  
-
   return (
     <nav className={`navbar ${navBackground}`}>
       <div className="section">
-      <Link className="navbar-title-link" to={'/dashboard'}>
-            <img src="images/cardinal-points.png" alt="Logo" className="navbar-logo " />
-            Innercompass
-          </Link>
-        <div className="navbar-links">
-          <Link className="nav-link" to={'/dashboard'}>Home</Link>
-          <Link className="nav-link" to={'/chat'}>Chat</Link>
+        <Link className="navbar-title-link" to={'/dashboard'}>
+          <img src="images/cardinal-points.png" alt="Logo" className="navbar-logo" />
+          Innercompass
+        </Link>
+        <div className={`navbar-links ${isMenuOpen ? 'open' : ''}`}>
+          <Link className="nav-link" to={'/dashboard'} onClick={toggleMenu}>Home</Link>
+          <Link className="nav-link" to={'/chat'} onClick={toggleMenu}>Chat</Link>
           <div className="navbar-buttons">
-          <button className="login-button"><Link to={'/'}>Logout</Link></button>
+            <button className="login-button"><Link to={'/'}>Logout</Link></button>
+          </div>
         </div>
+        <div className="menu-toggle" onClick={toggleMenu}>
+          <div className={`hamburger ${isMenuOpen ? 'open' : ''}`}></div>
         </div>
       </div>
     </nav>
